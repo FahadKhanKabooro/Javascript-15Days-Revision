fc61fa72b3fb36b905a075c8eaab09ca5dceb704
unknown <fahadkabooro.fk@gmail.com>
2023-04-09 23:24:35 +0500

    Class 7

---
+++ day 7/app.js
@@ -0,0 +1,40 @@
+                        //DOM
+
+// let Para = document.getElementById("para");
+// console.log(Para);
+
+// Para.innerHTML="Hello"
+
+// let pera = document.querySelector("#para");
+// pera.innerHTML = "<b> This is PeraGraph </b>"
+// console.log(pera)
+
+// let pera = document.querySelector("#para");
+// pera.innerHTML = "<b> This is PeraGraph </b>"
+// pera.style.backgroundColor = 'Blue';
+// pera.style.color = 'white';
+
+// setInterval(function(){
+//     if(pera.style.display === 'none'){
+//         pera.style.display = 'block';
+//     }else{pera.style.display = 'none'
+
+//     }
+// },1000);
+
+// let pera2 = document.querySelector('#para2');
+
+// pera2.innerHTML = "<b> This is PeraGraph </b>"
+// pera2.style.backgroundColor = 'Blue';
+// pera2.style.color = 'white';
+
+// setInterval(function(){
+//     if(pera2.style.width === '100%'){
+//         pera2.style.width = '10%'
+//     }else{pera2.style.width = '50%'
+
+//     }
+// },1000);
+
+
+
