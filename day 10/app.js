351dbb878941f29e7aeb288311506c8562ffdf25
unknown <fahadkabooro.fk@gmail.com>
2023-04-13 23:27:04 +0500

    Class 11

---
+++ day 10/app.js
@@ -0,0 +1,35 @@
+// syncronus Behavior => Line by Line
+
+// for(let i = 0; i < 50000;i++){
+    // console.log(i);
+// }
+
+// console.log("Hello Pakistan");
+
+//Asyncronus Behavior 
+
+// function runAfter1Sec(){
+//     setInterval(function(){
+// console.log("done")
+//     }, 2000)
+// }
+// runAfter1Sec();
+// console.log("HEllo Pakistan");
+
+//CallBacks..
+
+
+// function runAfter1Sec(cb){
+//     setInterval(function(){
+// let data = 5 + 123;
+// cb(data);
+//     }, 1000)
+// }
+
+
+// runAfter1Sec(function toBe(dataToGet){
+//     console.log("Yes , its Done ", dataToGet)
+//     });
+// console.log("HEllo Pakistan");
+
+//Promices
---
+++ day 11/app.js
@@ -0,0 +1,15 @@
+// HTTP Request
+// const imagesContainer = document.getElementById("images")
+
+
+
+// fetch('https://jsonplaceholder.typicode.com/posts')
+//       .then(response => response.json())
+//       .then(function(data){
+//         data.forEach(function(images) {
+//             const url = images.url;
+//             const imagesElem =  document.createElement("img");
+//             imagesElem.src = url;
+//             imagesContainer.appendChild(imagesElem);
+//         });;
+//       })
