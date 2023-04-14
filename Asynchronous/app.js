e0fba2d26c7c4ba3f3849581a300fbcad6abf627
unknown <fahadkabooro.fk@gmail.com>
2023-04-14 23:35:26 +0500

    Class 12

---
+++ Asynchronous/app.js
@@ -0,0 +1,15 @@
+// console.log("hello");
+// console.log("Javascript");
+
+
+// setTimeout(function(){
+// console.log("I am From SetTimeOut")
+// }, 3000);
+
+console.log("start");
+setTimeout(() =>{
+    console.log("This is a Set Time Out ")
+}, 2000);
+
+
+console.log("End");
---
+++ day 12/app.js
@@ -0,0 +1,92 @@
+// //      ES 6
+// // Arrow Functions
+// const add = (x,y) => {
+// return x + y
+// }
+
+// const sub = (x,y) => {
+//     return x - y
+//     }
+
+
+//     const obj = {
+//         name: 'Fahad',
+//         print: function(){
+//             setTimeout(() =>{
+//                 console.log(this.name);
+//             },2000)
+            
+//         }
+//     }
+
+// obj.print()
+
+// function printAll (x, y, ...z){ //Rest Parameter
+// console.log(x, y, z)
+// }
+// printAll(5,2,8,4,5,8,9);
+ 
+//Short Syntax for objects
+
+// const firstName = "ishaq";
+// const lastName = "bhojani";
+// const obj = {
+//     firstName,
+//     lastName,
+//     printName: function(){
+//         console.log(this.firstName,this.lastName)
+//     }
+    
+// };
+// // console.log(obj)
+// obj.printName()
+
+
+
+// const obj = {
+//         firstName: "fahad",
+//         lastName:"Khan",
+//         printName: function(){
+//             console.log("fahad Khan")
+//         }
+        
+//     };
+//     const {firstName} = obj;
+//     console.log(firstName)
+    // console.log(obj)
+
+    // const arr = ["Ishaq", "Bhojani"];
+    // const [,lastName] = arr;
+    // console.log(lastName)
+
+    //Spred Operators
+    // const arr = ["one","two","Three"];
+    // const newArr = ["Some Data",...arr];
+    // console.log(newArr)
+
+    // const obj = {
+    //     firstName: "Fahad",
+    //     lastNAme: "khan"
+    // };
+    // const newObj = {...obj,hobby:"Video Games",firstName:"Haseeb"};
+    // console.log(newObj)
+
+    // OOPS => Object Oriented Programoming
+    //CLAsses
+
+    // const obj = {
+    //     name:"LED",
+    //     price:12000,
+
+    // };
+    // const obj2 = {
+    //     title:"Samsung",
+    //     amount:14000
+    // };
+    // const products = [obj, obj2];
+    // products.forEach(p => console.log(p.name));
+
+
+   
+
+
